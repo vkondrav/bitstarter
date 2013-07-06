a21d43c865f3916a8e407994c43bb5be75f7dc56
vkondrav <kondrav@live.ca>
2013-07-06 20:30:13 +0000

    added index

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 
+var hello = fs.readFileSync ('index.html');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  response.send(hello)});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
