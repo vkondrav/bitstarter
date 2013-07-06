0fe3315b3600d2a25d5abd6f225fb04518d10cd3
vkondrav <kondrav@live.ca>
2013-07-06 21:13:19 +0000

    fix

--- web.js
+++ web.js
@@ -7,7 +7,8 @@ var hello_str = hello.toString("utf-8");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(hello_str)});
+  response.send(hello_str);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
